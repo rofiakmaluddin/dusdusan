a862e9e1a6e6c827e4ba1ecdc8bf95b838b19a4d
Rofi Akmaluddin <rofi.akmaluddin@gits.id>
2024-09-22 16:09:49 +0700

    feat: set font and color config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import ReduxProviders from '@/redux/provider';
-
-const inter = Inter({ subsets: ['latin'] });
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Dusdusan',
@@ -18,10 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ReduxProviders>
-          {children}
-        </ReduxProviders>
+      <Head>
+        <link
+          href="https://fonts.cdnfonts.com/css/neo-sans-pro"
+          rel="stylesheet"
+        />
+      </Head>
+      <body style={{ fontFamily: 'Neo Sans Pro' }}>
+        <ReduxProviders>{children}</ReduxProviders>
       </body>
     </html>
   );
